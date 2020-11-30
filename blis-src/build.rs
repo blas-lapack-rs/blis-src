71f41991d03dd34efef710d6eb1c8fd7130adb19
Jed Brown <jed@jedbrown.org>
2020-11-29 21:35:36 -0700

    Use Git submodule to obtain source
    * Upgrade to BLIS 0.8.0
      * Track arm32/arm64 deprecation (lack of run-time arch detection)
      * Enable static linking
      * Use MAKEFLAGS instead of manual make -j8
    * Remove the heavy build dependencies by using Git submodule

--- blis-src/build.rs
+++ blis-src/build.rs
@@ -1,11 +1,8 @@
-extern crate flate2;
-extern crate git2;
-extern crate reqwest;
-extern crate tar;
-
-const BLIS_VERSION:&str = "0.3.2";
-
-use std::{fs, path, process};
+use std::{
+    fs,
+    path::{Path, PathBuf},
+    process::Command,
+};
 
 fn env(k: &str) -> Option<String> {
     match std::env::var(k) {
@@ -14,44 +11,18 @@ fn env(k: &str) -> Option<String> {
     }
 }
 
-fn obtain_source(out_dir:&path::Path) {
-    let blis_build = out_dir.join("src");
-    if !blis_build.exists() {
-        if env("BLIS_SYS_GIT").is_some() || env("BLIS_SYS_GIT_BRANCH").is_some() {
-            git_clone(out_dir);
-        } else {
-            download_release(out_dir);
-        }
+fn compile(blis_build: &Path, out_dir: &Path) {
+    let mut configure = Command::new(blis_build.join("configure"));
+    configure
+        .current_dir(&blis_build)
+        .arg(format!("--prefix={}", out_dir.to_string_lossy()))
+        .arg("--enable-cblas")
+        .arg("--enable-threading=pthreads");
+    if env("CARGO_FEATURE_STATIC").is_some() {
+        configure.args(&["--enable-static", "--disable-shared"]);
+    } else {
+        configure.args(&["--disable-static", "--enable-shared"]);
     }
-}
-
-fn git_clone(out_dir:&path::Path) {
-    let mut repo = git2::build::RepoBuilder::new();
-    if let Some(b) = env("BLIS_SYS_GIT_BRANCH") {
-        repo.branch(&b);
-    }
-    repo.clone("https://github.com/flame/blis", &out_dir.join("src")).unwrap();
-}
-
-fn download_release(out_dir:&path::Path) {
-    let blis_release = format!("https://github.com/flame/blis/archive/{}.tar.gz", BLIS_VERSION);
-    let stream = reqwest::get(&blis_release).unwrap();
-    let stream = flate2::read::GzDecoder::new(stream);
-    tar::Archive::new(stream).unpack(&out_dir).unwrap();
-    fs::rename(
-        out_dir.join(format!("blis-{}", BLIS_VERSION)),
-        out_dir.join("src"),
-    ).unwrap();
-}
-
-fn compile(out_dir: &path::Path) {
-    let blis_build = out_dir.join("src");
-    let mut configure = process::Command::new(blis_build.join("configure"));
-    configure.current_dir(&blis_build)
-            .arg(format!("--prefix={}", out_dir.to_string_lossy()))
-            .arg("--enable-cblas")
-            .arg("--enable-shared")
-            .arg("--enable-threading=pthreads");
     if let Some(cc) = env("TARGET_CC") {
         configure.arg(format!("CC={}", cc));
     }
@@ -63,40 +34,53 @@ fn compile(out_dir: &path::Path) {
         "generic"
     } else {
         match &*rust_arch {
-            "x86_64" => "x86_64",
-            "arm"|"armv7" => "arm32",
-            "aarch64" => "arm64",
-            _ => "generic"
+            "x86_64" => "x86_64", // Build all microkernels; run-time dispatch
+
+            // BLIS does not have run-time arch detection on ARM or PowerPC.
+            // We'll let BLIS configure determine the best match.
+            "arm" | "armv7" => "auto", // cortexa9/cortexa15
+            "aarch64" => "auto",       // cortexa57/thunderx2
+            "powerpc64" => "auto",     // bgq/power9/power10
+            _ => "generic",
         }
     };
     configure.arg(arch);
-    assert!(configure
-            .status()
-            .unwrap()
-            .success()
-    );
-    assert!(
-        process::Command::new("make")
-            .arg("-j").arg("8")
-            .arg("install")
-            .current_dir(&blis_build)
-            .status()
-            .unwrap()
-            .success()
-    );
+    run(&mut configure);
+    let makeflags = env("CARGO_MAKEFLAGS").unwrap();
+    run(Command::new("make")
+        .arg("install")
+        .env("MAKEFLAGS", makeflags)
+        .current_dir(&blis_build));
 }
 
 fn main() {
-    let out_dir = path::PathBuf::from(env("OUT_DIR").unwrap());
+    let out_dir = PathBuf::from(env("OUT_DIR").unwrap());
     if env("CARGO_FEATURE_SYSTEM").is_none() {
         let lib_dir = out_dir.join("lib");
         let lib = lib_dir.join("libblis.a");
         if !lib.exists() {
-            obtain_source(&out_dir);
-            compile(&out_dir);
+            let target = env("TARGET").unwrap();
+            let build_dir = out_dir.join(format!("blis_{}", target.to_lowercase()));
+            if build_dir.exists() {
+                fs::remove_dir_all(&build_dir).unwrap();
+            }
+            run(Command::new("cp").arg("-R").arg("upstream").arg(&build_dir));
+            compile(&build_dir, &out_dir);
         }
-        println!("cargo:rustc-link-search=native={}", lib_dir.to_string_lossy());
+        println!(
+            "cargo:rustc-link-search=native={}",
+            lib_dir.to_string_lossy()
+        );
     }
-    let kind = if env("CARGO_FEATURE_STATIC").is_some() { "static" } else { "dylib" };
+    let kind = if env("CARGO_FEATURE_STATIC").is_some() {
+        "static"
+    } else {
+        "dylib"
+    };
     println!("cargo:rustc-link-lib={}=blis", kind);
+}
+
+fn run(command: &mut Command) {
+    println!("Running: `{:?}`", command);
+    assert!(command.status().unwrap().success());
 }
